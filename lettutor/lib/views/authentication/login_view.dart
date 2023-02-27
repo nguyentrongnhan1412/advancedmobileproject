451e20f2fcb080f9864ef28f66e896f4a6c233fd
NhanNguyen <64063604+nguyentrongnhan1412@users.noreply.github.com>
2023-02-28 00:02:00 +0700

    init constants + entities + loginview

---
+++ lettutor/lib/views/authentication/login_view.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:lettutor/constants/datatype.dart';
+import 'package:lettutor/constants/routes.dart';
+
+class LoginView extends StatefulWidget{
+  const LoginView({Key? key}) : super(key: key);
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  String chosenLanguage = Language.english;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
