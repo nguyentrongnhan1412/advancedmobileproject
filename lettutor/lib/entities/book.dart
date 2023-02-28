e10cb862a6eec7da00f057c8d8fb9a06fd6e9ca7
NhanNguyen <64063604+nguyentrongnhan1412@users.noreply.github.com>
2023-03-01 01:23:28 +0700

    add teacher card + data classes

---
+++ lettutor/lib/entities/book.dart
@@ -0,0 +1,7 @@
+class Book {
+  final String name;
+  final String description;
+  final String level;
+  final String imageUrl;
+  const Book(this.name, this.description, this.level, this.imageUrl);
+}
