de771adc8b9cb899bb790e51b95ea1af32c80f0d
NhanNguyen <64063604+nguyentrongnhan1412@users.noreply.github.com>
2023-02-28 16:50:41 +0700

    loginview + pubspec
    complete loginview
    update pubspec for assets dir

--- lettutor/lib/constants/styles.dart
+++ lettutor/lib/constants/styles.dart
@@ -5,32 +5,32 @@ final appTheme = ThemeData(
   primaryColor: Colors.blue,
   textTheme: const TextTheme(
 
-    headline1: TextStyle(
+    displayLarge: TextStyle(
       fontSize: 40,
       fontWeight: FontWeight.bold,
       color: Colors.blue,
       letterSpacing: 1.1
     ),
 
-    headline2: TextStyle(
+    displayMedium: TextStyle(
       fontSize: 28,
       fontWeight: FontWeight.w700,
       letterSpacing: 1.0,
       color: Colors.blue,
     ),
 
-    headline3: TextStyle(
+    displaySmall: TextStyle(
       fontSize: 22,
       fontWeight: FontWeight.bold,
       color: Colors.black,
     ),
 
-    headline4: TextStyle(
+    headlineMedium: TextStyle(
       fontSize: 18,
       fontWeight: FontWeight.bold,
       color: Colors.black,
     ),
 
-    bodyText1: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w400),
+    bodyLarge: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w400),
   ),
 );
--- lettutor/lib/main.dart
+++ lettutor/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:lettutor/constants/routes.dart';
+import 'package:lettutor/views/authentication/login_view.dart';
 
 void main() {
   runApp(const Lettutor());
@@ -16,7 +18,36 @@ class Lettutor extends StatelessWidget {
         useMaterial3: true,
         primaryColor: Colors.blue,
         scaffoldBackgroundColor: Colors.white,
-      )
+        textTheme: TextTheme(
+          displayLarge: TextStyle(
+              fontSize: 40,
+              fontWeight: FontWeight.bold,
+              color: Colors.blue[600],
+              letterSpacing: 1.1),
+          displayMedium: TextStyle(
+            fontSize: 26,
+            fontWeight: FontWeight.w700,
+            //letterSpacing: 1.0,
+            color: Colors.blue[600],
+          ),
+          displaySmall: const TextStyle(
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+          headlineMedium: const TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.w500,
+            color: Colors.black,
+          ),
+          bodyLarge: const TextStyle(fontSize: 16.0, fontWeight: FontWeight.w400),
+          bodyMedium: TextStyle(fontSize: 15.0, color: Colors.grey[700]),
+        ),
+      ),
+      home: const LoginView(),
+      routes: {
+        Routes.login: (context) => const LoginView(),
+      }
     );
   }
 }
