3add91393e5c4c8274ea0e2abed103fb6d0d18cb
NhanNguyen <64063604+nguyentrongnhan1412@users.noreply.github.com>
2023-03-01 17:23:50 +0700

    add courses page + hard-code data + course and book widgets

---
+++ lettutor/lib/widgets/course_card.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:lettutor/entities/course.dart';
+
+class CourseCard extends StatelessWidget {
+
+  final Course course;
+  final Function(String value)? onTap;
+
+  const CourseCard({Key? key,
+    required this.course,
+    this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        onTap!(course.name);
+      },
+
+      child: Card(
+        clipBehavior: Clip.hardEdge,
+        elevation: 2,
+        surfaceTintColor: Colors.white,
+        child: Column(
+          children: [
+            Text(
+              course.name,
+              style: Theme.of(context).textTheme.headline3,
+            ),
+
+            const SizedBox(
+              height: 8,
+            ),
+
+            Text(
+              course.description,
+              style: const TextStyle(fontWeight: FontWeight.w400, color: Colors.grey),
+            ),
+
+            const SizedBox(
+              height: 16,
+            ),
+
+            Row(
+              children: [
+                Expanded(
+                    child: Text(
+                      course.level,
+                      style: const TextStyle(fontSize: 18),
+                    )),
+
+                Text(
+                  '${course.numberOfLessons} lessons',
+                  style: const TextStyle(fontSize: 18),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+  
+}
