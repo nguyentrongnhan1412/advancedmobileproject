1e49e15f15a949ac593e718bbde2da29b081f4f6
NhanNguyen <64063604+nguyentrongnhan1412@users.noreply.github.com>
2023-03-01 18:19:00 +0700

    add homepage + init nav page

---
+++ lettutor/lib/pages/navigation_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:lettutor/constants/routes.dart';
+import 'package:lettutor/pages/courses_page.dart';
+import 'package:lettutor/pages/home_page.dart';
+
+class NavigationPage extends StatefulWidget {
+  const NavigationPage({Key? key}) : super(key: key);
+
+  @override
+  State<NavigationPage> createState() => _NavigationPageState();
+}
+
+class _NavigationPageState extends State<NavigationPage>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
