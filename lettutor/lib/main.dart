2c2426049d46b4d58613b4e2a98e9c3fa035505d
NhanNguyen <64063604+nguyentrongnhan1412@users.noreply.github.com>
2023-05-12 18:15:27 +0700

    edit main

--- lettutor/lib/main.dart
+++ lettutor/lib/main.dart
@@ -1,8 +1,15 @@
+import 'dart:io';
+import 'package:lettutor/src/providers/app_provider.dart';
+import 'package:lettutor/src/providers/auth_provider.dart';
+import 'package:provider/provider.dart';
+
 import 'package:flutter/material.dart';
 import 'package:lettutor/src/constants/routes.dart';
-import 'package:lettutor/src/views/authentication/login_view.dart';
-import 'package:lettutor/src/views/authentication/register_view.dart';
-import 'package:lettutor/src/views/authentication/forgot_password_view.dart';
+
+import 'package:lettutor/src/features/authentication/forgot_password_view.dart';
+import 'package:lettutor/src/features/authentication/login_view.dart';
+import 'package:lettutor/src/features/authentication/register_view.dart';
+
 import 'package:lettutor/src/pages/navigation_page.dart';
 import 'package:lettutor/src/views/course_detail_view.dart';
 import 'package:lettutor/src/views/become_tutor_view.dart';
@@ -13,64 +20,83 @@ import 'package:lettutor/src/views/user_profile_view.dart';
 import 'package:lettutor/src/views/video_call_view.dart';
 import 'package:lettutor/src/views/write_review_view.dart';
 
-void main() {
-  runApp(const Lettutor());
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (X509Certificate cert, String host, int port) => true;
+  }
 }
 
-class Lettutor extends StatelessWidget {
-  const Lettutor({super.key});
+void main() {
+  HttpOverrides.global = MyHttpOverrides();
+  runApp(const LetTutor());
+}
+
+class LetTutor extends StatelessWidget {
+  const LetTutor({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Lettutor',
-      theme: ThemeData(
-        useMaterial3: true,
-        colorScheme: const ColorScheme.light().copyWith(primary: Colors.pink),
-        // primaryColor: Colors.pink,
-        scaffoldBackgroundColor: Colors.white,
-        textTheme: TextTheme(
-          displayLarge: TextStyle(
-              fontSize: 40,
-              fontWeight: FontWeight.bold,
-              color: Colors.pink[600],
-              letterSpacing: 1.1),
-          displayMedium: TextStyle(
-            fontSize: 26,
-            fontWeight: FontWeight.w700,
-            color: Colors.pink[600],
-          ),
-          displaySmall: const TextStyle(
-            fontSize: 22,
-            fontWeight: FontWeight.bold,
-            color: Colors.black,
-          ),
-          headlineMedium: const TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.w500,
-            color: Colors.black,
-          ),
-          bodyLarge: const TextStyle(fontSize: 16.0, fontWeight: FontWeight.w400),
-          bodyMedium: TextStyle(fontSize: 15.0, color: Colors.grey[700]),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => AppProvider(),
         ),
-      ),
-      home: const LoginView(),
-      routes: {
-        Routes.login: (context) => const LoginView(),
-        Routes.register: (context) => const RegisterView(),
-        Routes.forgotPassword: (context) => const ForgotPasswordView(),
-        Routes.main: (context) => const NavigationPage(),
-        Routes.courseDetail: (context) => const CourseDetailView(),
-        Routes.becomeTutor: (context) => const BecomeTutorView(),
-        Routes.bookingDetail: (context) => const BookingDetailView(),
-        Routes.review: (context) => const TutorReviewView(),
-        Routes.writeReview: (context) => const WriteReviewView(),
-        Routes.videoCall: (context) => const VideoCallView(),
-        Routes.teacherDetail: (context) => const TeacherDetailView(),
-        Routes.userProfile: (context) => const UserProfileView(),
-      }
+        ChangeNotifierProvider(
+          create: (_) => AuthProvider(),
+        ),
+      ],
+      child: MaterialApp(
+          title: 'LetTutor',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            useMaterial3: true,
+            colorScheme: const ColorScheme.light().copyWith(primary: Colors.pink),
+            scaffoldBackgroundColor: Colors.white,
+            textTheme: TextTheme(
+              displayLarge: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.pink[600],
+                  letterSpacing: 1.1),
+              displayMedium: TextStyle(
+                fontSize: 26,
+                fontWeight: FontWeight.w700,
+                color: Colors.pink[600],
+              ),
+              displaySmall: const TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+              headlineMedium: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
+                color: Colors.black,
+              ),
+              bodyLarge: const TextStyle(fontSize: 16.0, fontWeight: FontWeight.w400),
+              bodyMedium: TextStyle(fontSize: 15.0, color: Colors.grey[700]),
+            ),
+          ),
+          home: const LoginView(),
+          routes: {
+            Routes.login: (context) => const LoginView(),
+            Routes.register: (context) => const RegisterView(),
+            Routes.forgotPassword: (context) => const ForgotPasswordView(),
+            Routes.main: (context) => const NavigationPage(),
+            Routes.becomeTutor: (context) => const BecomeTutorView(),
+            Routes.userProfile: (context) => const UserProfileView(),
+            Routes.courseDetail: (context) => const CourseDetailView(),
+            Routes.teacherDetail: (context) => const TutorDetailView(),
+            Routes.review: (context) => const TutorFeedbackView(),
+            Routes.writeReview: (context) => const WriteReviewView(),
+            Routes.tutorSearchResult: (context) => const TutorSearchResult(),
+          }
+      )
     );
+
   }
 }
 
